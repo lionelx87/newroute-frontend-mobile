ace4b22f6b66509880b6436b0e226d1dd948c3fa
lionelx87 <lionelx87@gmail.com>
2020-10-20 16:54:04 -0300

    Use ionViewWillEnter to update

--- src/app/pages/tour/tour.page.ts
+++ src/app/pages/tour/tour.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../../services/storage.service';
 import { Spot } from '../../interfaces/spot.interface';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tour',
@@ -13,7 +14,8 @@ export class TourPage implements OnInit {
   spots: Spot[];
 
   constructor( private storage: StorageService,
-               private router: Router ) { }
+               private router: Router,
+               private alertCtrl: AlertController ) { }
 
   ngOnInit() {
     this.getSpots();
@@ -24,9 +26,24 @@ export class TourPage implements OnInit {
   }
 
   toCategories() {
-    this.router.navigateByUrl('/categories', {
-      replaceUrl: true
+    // this.router.navigateByUrl('/categories', { replaceUrl: true });
+    this.router.navigateByUrl('/categories');
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
     });
+
+    await alert.present();
+  }
+
+  ionViewWillEnter() {
+    this.getSpots();
   }
 
 }

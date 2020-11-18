357ccaff48346de02fb9fed03d0549e1d4d0db3c
lionelx87 <lionelx87@gmail.com>
2020-11-18 18:29:45 -0300

    Functional Login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { NewUser } from '../interfaces/new-user.interface';
+import { UserAuth } from '../interfaces/user-auth.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,17 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  registerNewUser(user): Observable<any> {
+  registerNewUser(user: NewUser): Observable<any> {
     return this.http.post(environment.backend + '/register', user)
+      .pipe(
+        catchError( (err) => {
+          return throwError(err.error.errors);
+        })
+      );
+  }
+
+  login(user: UserAuth) {
+    return this.http.post(environment.backend + '/login', user)
       .pipe(
         catchError( (err) => {
           return throwError(err.error.errors);

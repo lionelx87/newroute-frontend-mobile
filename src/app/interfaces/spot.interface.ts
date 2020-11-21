ae95b01ca7b7fb234334dc0893baaf0194e73621
lionelx87 <lionelx87@gmail.com>
2020-11-21 14:25:36 -0300

    Phone in Spot

--- src/app/interfaces/spot.interface.ts
+++ src/app/interfaces/spot.interface.ts
@@ -3,10 +3,15 @@ export interface Spot {
     name: string;
     description: string;
     address: string;
+    phones: Phone[];
     latitude: string;
     longitude: string;
     images: string[];
     priority?: {
         value: number
     }
+}
+
+interface Phone {
+    number: string;
 }

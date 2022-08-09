770c3dbcbac9c1119a57c10d37865701bcec30b9
Lionel <lionelx87@gmail.com>
2022-08-09 00:28:43 -0300

    feat: visit register ui

--- src/app/pages/visit-register/visit-register.module.ts
+++ src/app/pages/visit-register/visit-register.module.ts
@@ -10,12 +10,15 @@ import { VisitRegisterPage } from './visit-register.page';
 
 import { ComponentsModule } from '../../components/components.module';
 
+import { PipesModule } from '../../pipes/pipes.module';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     ComponentsModule,
+    PipesModule,
     VisitRegisterPageRoutingModule
   ],
   declarations: [VisitRegisterPage]

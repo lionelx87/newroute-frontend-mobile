a068fc0ff4798bfe088e0c11214e8b73b009d8c2
Lionel <lionelx87@gmail.com>
2022-08-14 01:35:46 -0300

    feat: visits for user

--- src/app/pages/visits/visits.page.ts
+++ src/app/pages/visits/visits.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from "rxjs";
+import { Spot } from "src/app/interfaces/spot.interface";
+import { VisitService } from 'src/app/services/visit.service';
 
 @Component({
   selector: 'app-visits',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visits.page.scss'],
 })
 export class VisitsPage implements OnInit {
+  spots: Observable<Spot[]>;
+  slideOpts = {
+    autoplay: {
+      delay: 3000,
+    },
+  };
 
-  constructor() { }
+  constructor(private visitService: VisitService) { }
 
   ngOnInit() {
+    this.visitService.getVisits().subscribe( (data: any) => {
+      const { visits } = data;
+      this.spots = of(visits);
+    });
   }
 
 }
--- src/app/services/visit.service.ts
+++ src/app/services/visit.service.ts
@@ -2,20 +2,33 @@ import { Injectable } from "@angular/core";
 import { environment } from "./../../environments/environment";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { AuthService } from "./auth.service";
+import { Spot } from "../interfaces/spot.interface";
 
 @Injectable({
   providedIn: "root",
 })
 export class VisitService {
-  
   constructor(private http: HttpClient, private auth: AuthService) {}
-  
+
   register(visits: number[]) {
     return this.http.post(
-      environment.backend + '/visits',
+      environment.backend + "/visits",
       { visits: JSON.stringify(visits) },
-      { headers: new HttpHeaders().append('Authorization', `Bearer ${this.auth.user.token}`) }
+      {
+        headers: new HttpHeaders().append(
+          "Authorization",
+          `Bearer ${this.auth.user.token}`
+        ),
+      }
     );
   }
 
+  getVisits() {
+    return this.http.get<Spot[]>(environment.backend + "/visits", {
+      headers: new HttpHeaders().append(
+        "Authorization",
+        `Bearer ${this.auth.user.token}`
+      ),
+    });
+  }
 }

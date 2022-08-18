a677e61c37d2be1fbeba2939c204dc00ca863868
Lionel <lionelx87@gmail.com>
2022-08-18 00:40:32 -0300

    add: settings page translate

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -8,6 +8,7 @@ import { SettingsPageRoutingModule } from './settings-routing.module';
 
 import { SettingsPage } from './settings.page';
 import { ComponentsModule } from '../../components/components.module';
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { ComponentsModule } from '../../components/components.module';
     FormsModule,
     IonicModule,
     ComponentsModule,
+    TranslateModule,
     SettingsPageRoutingModule
   ],
   declarations: [SettingsPage]
